affc561e9798426dbd37d256388c27c8433ac595
Pandey <Vikash.Pandey@FISGLOBAL.COM>
2020-09-04 15:56:27 +0530

    fix to make it run as jar

--- src/main/java/id/test/springboottesting/SpringBootTestingApplication.java
+++ src/main/java/id/test/springboottesting/SpringBootTestingApplication.java
@@ -2,9 +2,10 @@ package id.test.springboottesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootTestingApplication {
+public class SpringBootTestingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestingApplication.class, args);
